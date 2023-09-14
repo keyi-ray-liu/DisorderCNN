fix for eig, sign on t, various improvement

ED_src/gen.py
@@ -18,7 +18,7 @@ def generateState(L, num_e, qe):
     if len(num_e) == 1:
 
         num_e = num_e[0]
-        full = recur(L, num_e)
+        full = [ np.array(s) for s in recur(L, num_e)]
 
     elif len(num_e) == 2:
 
@@ -30,6 +30,8 @@ def generateState(L, num_e, qe):
 
     for _ in range(qe):
         full = [state + [0] for state in full] + [state + [1] for state in full]
+
+    print(len(full))
     return full
         
 

ED_src/hamiltonian.py
@@ -50,7 +50,7 @@ def hamiltonian(s, dis, para):
 
                 # jordan wigner string for cc operator
                 seg = s[ loc + 1: site]
-                jw = (-1) ** (seg[seg >0 ].shape[0] + seg[seg == 3].shape[0])
+                jw = (-1) ** (seg[seg >0 ].shape[0] - seg[seg == 3].shape[0])
                 if tun:
                     dx = - dis[0][loc]  -xloc + dis[0][site] + xs
                     dy = - dis[1][loc] -yloc + dis[1][site] + ys
@@ -63,9 +63,9 @@ def hamiltonian(s, dis, para):
                     else:
                         factor = np.sin( dr )
                     #print(factor)
-                    ts.append( -t * factor * jw)
+                    ts.append( t * factor * jw)
                 else:
-                    ts.append(- t * jw)
+                    ts.append( t * jw)
                 
 
         # 2D lattice, hop down
@@ -256,11 +256,19 @@ def hamiltonian(s, dis, para):
             allnewstates[1].append(newstate[i])
 
 
-        # the ee interaction part, the 0.5 is for the double counting of sites. 
-        allee += ee(loc) 
+    # the ee interaction part, the 0.5 is for the double counting of sites.
+    #
+        
+    if int_ee != 0:
+
+        for loc in range(L):
+            allee += ee(loc) 
+    
+    if int_ne != 0:
         # the ne interaction part
-        allne += ne(loc)
-        #print(ne(row, col))
+
+        for loc in range(L):
+            allne += ne(loc)
         
 
     #print(allee, allne)

ED_src/set.py
@@ -1,8 +1,9 @@
 from collections import defaultdict
 from hamiltonian import *
 from scipy.sparse import csr_matrix, load_npz, save_npz
-from scipy.special import comb
 import os
+from utils import *
+
 
 def setMatrix(S, N, dis, sdict, para, tag=''):
 
@@ -16,14 +17,16 @@ def setMatrix(S, N, dis, sdict, para, tag=''):
 
         #print(cnt)
 
-    frac = N // 100
+    frac = N // 100 if N >= 100 else 1
     cnt = 0
+
+    sparse_M = 'M{}.npz'.format(tag)
+    dense_M = 'M{}'.format(tag)
 
     if sparse:
 
-        matrix_file = 'M{}.npz'.format(tag)
-        if os.path.exists(matrix_file) and readM:
-            M = load_npz(matrix_file)
+        if os.path.exists(sparse_M) and readM:
+            M = load_npz(sparse_M)
 
         else:
             row = []
@@ -43,13 +46,12 @@ def setMatrix(S, N, dis, sdict, para, tag=''):
                     val += [newstates[0][j]]
 
             M = csr_matrix((val, (row, col)), shape=(N, N))
-            save_npz(matrix_file, M)
+            save_npz(sparse_M, M)
 
     else:
 
-        matrix_file = 'M'
-        if os.path.exists(matrix_file) and readM:
-            M = np.loadtxt(matrix_file)
+        if os.path.exists(dense_M) and readM:
+            M = np.loadtxt(dense_M)
 
         else:
             M = np.zeros((N, N))
@@ -64,28 +66,17 @@ def setMatrix(S, N, dis, sdict, para, tag=''):
                 for j, newstate  in enumerate(newstates[1]):
                     M[i, sdict[str(newstate)]] += newstates[0][j]
 
-            np.savetxt('M', M, fmt='%.3f')
+            np.savetxt(dense_M + 'readable', M, fmt='%.3f')
+            np.savetxt(dense_M, M)
             
-            #checkdiag(M, para)
+            #\checkdiag(M, para)
             checksparce()
+
+    #if os.path.exists(sparse_M) and os.path.exists(dense_M):
+    #    check_same(sparse_M, dense_M)
     
     return M
    
-def checkdiag(M, para):
 
-    L = para['L']
-    num_e = para['num_e']
-
-    num = int(comb(L, num_e))
-    rep = int(M.shape[0] // num)
-    
-
-    for r in range(rep):
-        for c in range(rep):
-
-            block = M[ r * num : (r + 1) * num, c * num: (c + 1)*num]
-            zeros = np.count_nonzero( block - np.diag(np.diagonal(block)))
-
-            print(r, c, zeros)
 
 